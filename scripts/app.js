706d48d57c3bf0b9ae29ff8e8c24a716770991c5
TorrieL <hmminicity2@163.com>
2019-07-20 19:58:08 +0800

    changing username& local storage

--- scripts/app.js
+++ scripts/app.js
@@ -1,6 +1,8 @@
 //dom queries
 const chatList = document.querySelector('.chat-list');
 const newChatForm = document.querySelector('.new-chat');
+const newNameForm = document.querySelector('.new-name');
+const updateMsg = document.querySelector('.update-msg');
 
 //add a new chat
 newChatForm.addEventListener('submit', e => {
@@ -13,11 +15,27 @@ newChatForm.addEventListener('submit', e => {
     }).catch((err) => {
       console.log(err)
     })
-})
+});
+
+//update username
+newNameForm.addEventListener('submit', e => {
+  e.preventDefault();
+  //update new name via chatroom
+  const newName = newNameForm.name.value.trim();
+  chatroom.updateName(newName);
+  //reset the form
+  newNameForm.reset();
+  //show then hide the update msg
+  updateMsg.innerText = `Your name has been updated to ${newName}`;
+  setTimeout(() =>updateMsg.innerText = '',3000);
+});
+
+//check local storage for a name
+const username = localStorage.username ? localStorage.username : 'anonymous';
 
 //class instances
 const chatUI = new ChatUI(chatList);
-const chatroom = new Chatroom('eco', 'chimp');
+const chatroom = new Chatroom('eco', username);
 
 //get chats and render
 chatroom.getChats(data => chatUI.render(data));
